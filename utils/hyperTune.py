39d579c7698949e3a51538cc02f98eccb98df6a4
emna <emna.amor8@gmail.com>
2020-02-09 17:40:08 +0100

    enable learning parameters tuning :sparkles:

---
+++ utils/hyperTune.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+from itertools import product
+
+
+# Read in the hyper-parameters and return a Run namedtuple containing all the
+# combinations of hyper-parameters
+class RunBuilder():
+  @staticmethod
+  def get_runs(params):
+    Run = namedtuple('Run', params.keys())
+    runs = []
+    for v in product(*params.values()):
+      runs.append(Run(*v))
+
+    return runs
