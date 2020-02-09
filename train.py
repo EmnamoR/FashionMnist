e51942e4bf5ab4e5566611a12eb832257d9ad2a5
emna <emna.amor8@gmail.com>
2020-02-09 17:37:10 +0100

    change structure :art:

--- train.py
+++ train.py
@@ -37,6 +37,51 @@ class trainer(object):
     # self.optimizer = optim.Adam(self.model.parameters(), lr=run.lr)
     self.optimizer = torch.optim.SGD(model.parameters(), lr=0.001,
                                   momentum=0.9, nesterov=True)
+    # scheduler = StepLR(self.optimizer, step_size=2, gamma=0.1)
+
+    train_losses, test_losses = [], []
+
+    for epoch in range(self.config.epochs):
+        train_loss = 0
+        test_loss = 0
+        accuracy = 0
+        for images, labels in train_loader:
+            images = images.to(self.device)
+            labels = labels.to(self.device)
+            self.optimizer.zero_grad()
+            op = model(images)
+            loss = self.criterion(op, labels)
+            train_loss += loss.item()
+            loss.backward()
+            self.optimizer.step()
+        else:
+            with torch.no_grad():
+                model.eval()
+                for images, labels in validation_loader:
+                    images = images.to(self.device)
+                    labels = labels.to(self.device)
+                    log_ps = model(images)
+                    prob = torch.exp(log_ps)
+                    top_probs, top_classes = prob.topk(1, dim=1)
+                    equals = labels == top_classes.view(labels.shape)
+                    accuracy += equals.type(torch.FloatTensor).mean()
+                    test_loss += self.criterion(log_ps, labels)
+            model.train()
+        train_loss_e = train_loss / len(train_loader)
+        test_loss_e = test_loss / len(validation_loader)
+        accuracy_e = accuracy / len(validation_loader)
+        print("Epoch: {}/{}.. ".format(epoch + 1, self.config.epochs),
+              "Training Loss: {:.3f}.. ".format(train_loss_e),
+              "Test Loss: {:.3f}.. ".format(test_loss_e),
+              "Test Accuracy: {:.3f}".format(accuracy_e))
+        train_losses.append(train_loss_e)
+        test_losses.append(test_loss_e)
+
+
+if __name__ == '__main__':
+  t = trainer()
+  t.run()
+
 
 
 
