758ec72ab536e468ea5bdc9e007d6d69fb81da6d
emna <emna.amor8@gmail.com>
2020-02-10 13:34:32 +0100

    improve code pep8 conv :art:

--- utils/hyperTune.py
+++ utils/hyperTune.py
@@ -1,15 +1,28 @@
+"""
+Scripts enable to read in the hyper-parameters
+and return a Run namedtuple containing
+all the combinations of hyper-parameters
+"""
 from collections import namedtuple
 from itertools import product
 
 
-# Read in the hyper-parameters and return a Run namedtuple containing all the
-# combinations of hyper-parameters
 class RunBuilder:
-  @staticmethod
-  def get_runs(params):
-    Run = namedtuple('Run', params.keys())
-    runs = []
-    for v in product(*params.values()):
-      runs.append(Run(*v))
+    """
+    RunBuilder get the combinations of hyper-parameters
+    """
 
-    return runs
+    @staticmethod
+    def get_runs(params):
+        """
+        Parameters:
+        param (Orderdict): contains all network param that we need to tune
+        Returns:
+        runs: list containing all runs with different parameters
+       """
+        Run = namedtuple('Run', params.keys())
+        runs = []
+        for val in product(*params.values()):
+            runs.append(Run(*val))
+
+        return runs
