b0e3f00d307792cd24da24e6fcf9302a33deb094
emna <emna.amor8@gmail.com>
2020-02-10 13:35:27 +0100

    improve code pep8 conv :art:

--- utils/logger.py
+++ utils/logger.py
@@ -1,21 +1,33 @@
+"""
+Script to make Python logging using the logging module automatically output to stdout
+"""
 import logging
 import sys
 
-class logger(object):
-  def __init__(self):
-    self.LOGGING_FORMAT = '[%(pathname)s][%(funcName)s:%(lineno)d]' + \
-                       '[%(levelname)s] %(message)s'
-    self.LOGGING_STREAM = sys.stdout
 
-  def get_logger(self, logger_name, log_level=logging.DEBUG):
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(log_level)
-    ch = logging.StreamHandler(self.LOGGING_STREAM)
-    formatter = logging.Formatter(self.LOGGING_FORMAT)
-    ch.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    ch.setLevel(log_level)
-    logger.addHandler(ch)
-    logger.propagate = False
+class Logger:
+    """
+      Custom logging class with stdout stream handler enabled
+    """
 
-    return logger
+    def __init__(self):
+        self.logging_format = '[%(pathname)s][%(funcName)s:%(lineno)d]' + \
+                              '[%(levelname)s] %(message)s'
+        self.logging_stream = sys.stdout
+
+    def get_logger(self, logger_name, log_level=logging.DEBUG):
+        """
+        configure a stream handler (using stdout instead of the default stderr)
+        and add it to the root logger
+        """
+        lo_gger = logging.getLogger(logger_name)
+        lo_gger.setLevel(log_level)
+        stream_handler = logging.StreamHandler(self.logging_stream)
+        formatter = logging.Formatter(self.logging_format)
+        stream_handler.setFormatter(formatter)
+        stream_handler.setFormatter(formatter)
+        stream_handler.setLevel(log_level)
+        lo_gger.addHandler(stream_handler)
+        lo_gger.propagate = False
+
+        return lo_gger
