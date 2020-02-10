97931f20218a2136ffc521b96e293daeefe85746
emna <emna.amor8@gmail.com>
2020-02-10 13:36:12 +0100

    improve code pep8 conv :art:

--- dataloader.py
+++ dataloader.py
@@ -1,11 +1,16 @@
+"""
+Script to load data from ubyte files
+"""
 import idx2numpy
 import numpy as np
 from torch.utils.data.sampler import SubsetRandomSampler
-
 from dataset import CustomDatasetFromArrays
 
-
 class DataLoader:
+    """
+    Dataloader with random sampler
+    to split the data into training and validation
+    """
     def __init__(self, config=None):
         self.config = config
         self.images = idx2numpy.convert_from_file(self.config.images_path)
@@ -13,7 +18,11 @@ class DataLoader:
         self.dataset_size = len(self.images)
         assert self.dataset_size == len(self.labels)
 
-    def getLoaders(self, shuffle_dataset=False):
+    def get_loaders(self, shuffle_dataset=False):
+        """
+        :param shuffle_dataset(bool): data shuffle enabled / disabled
+        :return: dataset, train_sampler, validation_sampler
+        """
         dataset = CustomDatasetFromArrays(self.images, self.labels, True)
         indices = list(range(self.dataset_size))
         split = int(np.floor(self.config.validation_split * self.dataset_size))
