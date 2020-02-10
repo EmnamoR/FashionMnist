4d43281256ba6f0fbdde01c00942d55c12756e8b
emna <emna.amor8@gmail.com>
2020-02-10 13:35:51 +0100

    improve code pep8 conv :art:

--- utils/tflogs.py
+++ utils/tflogs.py
@@ -1,17 +1,31 @@
+"""
+Script to insert model training metrics
+to the tensorflow tensorboard
+"""
 from torch.utils.tensorboard import SummaryWriter
+class TfLogWriter:
+    """
+    Log metrics to tensorboard
+    and calculate training time
+    """
+    def __init__(self):
+        self.tboard = None
 
-class logWriter():
-  def __init__(self):
-    self.tb = None
+    def begin_run(self, run):
+        """
+        :param run: run containing custom param
+        write to tensorboard
+        """
+        self.tboard = SummaryWriter(comment=f'-{run.models.__class__.__name__}')
 
-  # save for each run params
-  def begin_run(self, run):
-    self.tb = SummaryWriter(comment=f'-{run}')
-
-
-  def add_to_board(self, train_loss, test_loss, acc, num_epoch):
-    # Record epoch loss and accuracy to TensorBoard
-    self.tb.add_scalar('Test Loss', test_loss, num_epoch)
-    self.tb.add_scalar('Train Loss', train_loss, num_epoch)
-    self.tb.add_scalar('Accuracy', acc, num_epoch)
-
+    def add_to_board(self, train_loss, test_loss, acc, num_epoch):
+        """
+        :param train_loss: epoch training loss
+        :param test_loss: epoch validation loss
+        :param acc: epoch validation accuracy
+        :param num_epoch: the number of the current epoch
+        :return:  Record epoch loss and accuracy to TensorBoard
+        """
+        self.tboard.add_scalar('Test Loss', test_loss, num_epoch)
+        self.tboard.add_scalar('Train Loss', train_loss, num_epoch)
+        self.tboard.add_scalar('Accuracy', acc, num_epoch)
